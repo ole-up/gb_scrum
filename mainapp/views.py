647c7943307131c48759e6777ae9bf9658138ff5
AndyMcMagadan <mcmagadan@mail.ru>
2022-05-30 13:46:12 +0300

    First step

---
+++ mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, "mainapp/index.html")
+
+
+def products(request):
+    return render(request, "mainapp/products.html")
+
+
+def contact(request):
+    return render(request, "mainapp/contact.html")
