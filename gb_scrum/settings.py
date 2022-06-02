e03b5d603d01d4a99ae29fd3ed9f62f6318bb190
AndyMcMagadan <mcmagadan@mail.ru>
2022-06-02 17:28:53 +0300

    Migrate done

--- gb_scrum/settings.py
+++ gb_scrum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,9 +126,12 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.timezone import datetime
 
-# Create your models here.
+
+class HabrCategory(models.Model):
+    name = models.CharField(verbose_name="Категория",
+                            max_length=64, unique=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    category_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Habr(models.Model):
+    category = models.ForeignKey(HabrCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="Заголовок", max_length=70)
+    image = models.ImageField(upload_to="article_images", blank=True)
+    short_desc = models.CharField(
+        verbose_name="Аннотация", max_length=256, blank=True)
+    description = models.TextField(
+        verbose_name="Содержание", blank=True)
+    article_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
